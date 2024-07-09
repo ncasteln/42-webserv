92960d937d5c76d7eeb1c0cd88501c9d4fc12134
nnavidd <navid.nabaeei@gmail.com>
2024-07-09 03:29:36 +0200

    style: fixing some indentation and make clear some parts

--- src/HttpRequest.cpp
+++ src/HttpRequest.cpp
@@ -6,109 +6,101 @@
 /*   By: nnavidd <nnavidd@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 10:39:02 by nnabaeei          #+#    #+#             */
-/*   Updated: 2024/07/09 01:24:38 by nnavidd          ###   ########.fr       */
+/*   Updated: 2024/07/09 03:24:40 by nnavidd          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HttpRequest.hpp"
 
-#include <unistd.h>    // For read, write, close
-#include <iostream>    // For std::cout, std::endl
-#include <cstdio>      // For perror
-#include <cstring>     // For memset
-#include <sstream>     // For std::istringstream
-#include <algorithm>   // For std::remove
-#include "HttpRequest.hpp"
-
-HTTPRequest::HTTPRequest(const std::string& req) : request(req) {}
+HTTPRequest::HTTPRequest(const std::string& req) : _request(req) {}
 
 bool HTTPRequest::isValidMethod(const std::string& mthd) {
-    return (mthd == "GET" || mthd == "POST" || mthd == "HEAD");
+	return (mthd == "GET" || mthd == "POST" || mthd == "HEAD");
 }
 
 bool HTTPRequest::isValidHttpVersion(const std::string& ver) {
-    return (ver == "HTTP/1.1" || ver == "HTTP/1.0");
+	return (ver == "HTTP/1.1" || ver == "HTTP/1.0");
 }
 
 bool HTTPRequest::parse() {
-    std::istringstream requestStream(request);
-    std::string line;
+	std::istringstream requestStream(_request);
+	std::string line;
 
-    // Parse request line
-    if (!std::getline(requestStream, line)) {
-        return false;
-    }
-    std::istringstream requestLine(line);
-    if (!(requestLine >> method >> uri >> version)) {
-        return false;
-    }
+	// Parse request line
+	if (!std::getline(requestStream, line)) {
+		return false;
+	}
+	std::istringstream requestLine(line);
+	if (!(requestLine >> _method >> _uri >> _version)) {
+		return false;
+	}
 
-    if (!isValidMethod(method) || !isValidHttpVersion(version)) {
-        return false;
-    }
+	if (!isValidMethod(_method) || !isValidHttpVersion(_version)) {
+		return false;
+	}
 
-    // Parse headers
-    while (std::getline(requestStream, line) && line != "\r") {
-        size_t pos = line.find(":");
-        if (pos != std::string::npos) {
-            std::string key = line.substr(0, pos);
-            std::string value = line.substr(pos + 1);
-            value.erase(std::remove(value.begin(), value.end(), '\r'), value.end());
-            value.erase(0, value.find_first_not_of(" "));
-            headers[key] = value;
-        }
-    }
+	// Parse headers
+	while (std::getline(requestStream, line) && line != "\r") {
+		size_t pos = line.find(":");
+		if (pos != std::string::npos) {
+			std::string key = line.substr(0, pos);
+			std::string value = line.substr(pos + 1);
+			value.erase(std::remove(value.begin(), value.end(), '\r'), value.end());
+			value.erase(0, value.find_first_not_of(" "));
+			headers[key] = value;
+		}
+	}
 
-    return true;
+	return true;
 }
 
 int HTTPRequest::validate() {
-    if (headers.find("Host") == headers.end()) {
-        return 400;
-    }
-    return 200;
+	if (headers.find("Host") == headers.end()) {
+		return 400;
+	}
+	return 200;
 }
 
 std::string HTTPRequest::getResponse() {
-    int statusCode = validate();
+	int statusCode = validate();
 
-    if (statusCode == 200) {
-        return httpStatusCode(200) + "Content-Type: text/html\r\n\r\n<html><body><h1>Success</h1></body></html>";
-    } else {
-        return httpStatusCode(400) + "Content-Type: text/html\r\n\r\n<html><body><h1>Bad Request</h1></body></html>";
-    }
+	if (statusCode == 200) {
+		return httpStatusCode(200) + "Content-Type: text/html\r\n\r\n<html><body><h1>Success</h1></body></html>";
+	} else {
+		return httpStatusCode(400) + "Content-Type: text/html\r\n\r\n<html><body><h1>Bad Request</h1></body></html>";
+	}
 }
 
 std::string HTTPRequest::httpStatusCode(int statusCode) {
-    switch (statusCode)
-    {
-        case 200 : return "HTTP/1.1 200 OK\r\n";
-        case 400 : return "HTTP/1.1 400 Bad Request\r\n";
-        case 404 : return "HTTP/1.1 404 Not Found\r\n";
-        default: return "HTTP/1.1 500 Internal Server Error";
-    }
+	switch (statusCode)
+	{
+		case 200 : return "HTTP/1.1 200 OK\r\n";
+		case 400 : return "HTTP/1.1 400 Bad Request\r\n";
+		case 404 : return "HTTP/1.1 404 Not Found\r\n";
+		default: return "HTTP/1.1 500 Internal Server Error";
+	}
 }
 
 void HTTPRequest::handleRequest(int clientSocket) {
-    char buffer[1024];
-    ssize_t bytesRead = read(clientSocket, buffer, sizeof(buffer) - 1);
-    if (bytesRead < 0) {
-        perror("read");
-        close(clientSocket);
-        return;
-    }
-    buffer[bytesRead] = '\0';
-    request = buffer;
+	char buffer[1024];
+	ssize_t bytesRead = read(clientSocket, buffer, sizeof(buffer) - 1);
+	if (bytesRead < 0) {
+		perror("read");
+		close(clientSocket);
+		return;
+	}
+	buffer[bytesRead] = '\0';
+	_request = buffer;
 
-    HTTPRequest httpRequest(request);
+	HTTPRequest httpRequest(_request);
 
-    if (!httpRequest.parse()) {
-        std::string response = "HTTP/1.1 400 Bad Request\r\nContent-Type: text/html\r\n\r\n<html><body><h1>Bad Request</h1></body></html>";
-        write(clientSocket, response.c_str(), response.length());
-    } else {
-        std::string response = httpRequest.getResponse();
-        write(clientSocket, response.c_str(), response.length());
-    }
+	if (!httpRequest.parse()) {
+		std::string response = "HTTP/1.1 400 Bad Request\r\nContent-Type: text/html\r\n\r\n<html><body><h1>Bad Request</h1></body></html>";
+		write(clientSocket, response.c_str(), response.length());
+	} else {
+		std::string response = httpRequest.getResponse();
+		write(clientSocket, response.c_str(), response.length());
+	}
 
-    close(clientSocket);
+	close(clientSocket);
 }
--- src/HttpRequest.hpp
+++ src/HttpRequest.hpp
@@ -6,47 +6,44 @@
 /*   By: nnavidd <nnavidd@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 02:33:30 by nnabaeei          #+#    #+#             */
-/*   Updated: 2024/07/09 01:15:11 by nnavidd          ###   ########.fr       */
+/*   Updated: 2024/07/09 03:23:43 by nnavidd          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HTTPREQUEST_HPP
 # define HTTPREQUEST_HPP
 
-#include <iostream>
 #include <string>
 #include <map>
-#include <sstream>
-#include <algorithm>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
-#include <unistd.h>
-
-// HTTP status codes
-const std::string HTTP_200 = "HTTP/1.1 200 OK\r\n";
-const std::string HTTP_400 = "HTTP/1.1 400 Bad Request\r\n";
-const std::string HTTP_404 = "HTTP/1.1 404 Not Found\r\n";
+#include <unistd.h>    // For read, write, close
+#include <iostream>    // For std::cout, std::endl
+#include <cstdio>      // For perror
+#include <cstring>     // For memset
+#include <sstream>     // For std::istringstream
+#include <algorithm>   // For std::remove
 
 
 class HTTPRequest {
 public:
-    HTTPRequest(const std::string& request);
-    bool parse();
-    int validate();
-    std::string getResponse();
-    void handleRequest(int clientSocket);
+	HTTPRequest(const std::string& request);
+	bool parse();
+	int validate();
+	std::string getResponse();
+	void handleRequest(int clientSocket);
 
 private:
-    std::string request;
-    std::string method;
-    std::string uri;
-    std::string version;
-    std::map<std::string, std::string> headers;
+	std::string _request;
+	std::string _method;
+	std::string _uri;
+	std::string _version;
+	std::map<std::string, std::string> headers;
 
-    bool isValidMethod(const std::string& method);
-    bool isValidHttpVersion(const std::string& version);
-    std::string  httpStatusCode(int code);
+	bool isValidMethod(const std::string& method);
+	bool isValidHttpVersion(const std::string& version);
+	std::string  httpStatusCode(int code);
 };
 
 #endif
